8518b3316980381f186fc03a15319876a53d30be
KJ <xiankai@gmail.com>
2017-05-14 14:03:20 +0800

    Assets

---
+++ constants/Brands.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: 'KFC',
+    image: require('../assets/images/kfc.png'),
+  },
+  {
+    name: 'McDonalds',
+    image: require('../assets/images/mcd.jpg'),
+  },
+  {
+    name: 'Subway',
+    image: require('../assets/images/subway.jpg'),
+  },
+];
